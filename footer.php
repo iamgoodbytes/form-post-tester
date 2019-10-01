01cdcd44a6cfa1808cad8de55025c09c8c36a559
Joris Hens <joris@goodbytes.be>
2019-10-01 10:44:41 +0200

    removed key

--- footer.php
+++ footer.php
@@ -1,5 +1,4 @@
 <footer>
     <p>This form POST tester was initially created for my <a href="https://www.weareimd.be">web development students</a> but can be handy for anyone learning about HTML forms.</p>
     <p>Code for this tool is open source and can be found on <a href="https://github.com/iamgoodbytes/form-post-tester">github</a> (expect nothing fancy). Find me at <a href="https://www.goodbytes.be" title="Security &amp; Privacy focused web developer">GoodBytes</a>.</p>
-    <p>☕️ Tip a coffee / Bitcoin Satoshi: <span>1PepVE6gxT6gpVJ3RbWDEz9ncFJCyf75AX</span></p>
 </footer>
